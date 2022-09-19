c0c990e59c4750ec443d2dd03d0ba3d9180b6dc2
Yafi <107351013+yafiyafi@users.noreply.github.com>
2022-09-19 16:39:10 +0300

    Update API_test.py

--- API_test.py
+++ API_test.py
@@ -1,18 +1,26 @@
 import loginPage
 import cartPage
 
-userToken=loginPage.login().loginUser("Yafi","108010")
-cart=cartPage.cart()
+cart = cartPage.cart()
+user = loginPage.login()
+
+def signUpUserAndloginUserAndaddToCartItem():
+    user.signUpUser()
+    userToken = user.loginUser()
+    user.userToken = userToken
+    cart.addToCartItem(user.userToken)
+
+signUpUserAndloginUserAndaddToCartItem()
 
 def testIfNumberOfItemsInTheCardIsOne():
-    numberOfItemsInTheCard = len(cart.viewAllTheProductsInTheCart(userToken))
+    numberOfItemsInTheCard = len(cart.viewAllTheProductsInTheCart(user.userToken))
     assert numberOfItemsInTheCard == 1
 
 def testIfItemIdEqual3():
-    idItem = cart.viewAllTheProductsInTheCart(userToken)[0]["prod_id"]
+    idItem = cart.viewAllTheProductsInTheCart(user.userToken)[0]["prod_id"]
     cart.idprod = idItem
     assert idItem == 3
-testIfItemIdEqual3()
+
 def testIfThePriceOfTheSelectedPhoneEqual650():
     priceOfItem = cart.viewDetailsOfAProduct(cart.idprod)["price"]
     assert priceOfItem == 650
