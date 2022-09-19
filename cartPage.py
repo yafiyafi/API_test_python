7cff2bf22d514dec9e4e9fc4ff5098bc7faaa80b
Yafi <107351013+yafiyafi@users.noreply.github.com>
2022-09-19 16:37:29 +0300

    Update cartPage.py

--- cartPage.py
+++ cartPage.py
@@ -1,12 +1,27 @@
 import requests
 import json
+
 class cart:
     def __init__(self):
         self.url = 'https://api.demoblaze.com/'
         self.idprod=""
-    def viewAllTheProductsInTheCart(self,userName):
+    def addToCartItem(self,userToken):
+        with open("items.txt","r") as file:
+            item = file.readline()
         payload = json.dumps({
-            "cookie": str(userName),
+            "id": json.loads(item)["id"],
+            "cookie": userToken,
+            "prod_id": json.loads(item)["prod_id"],
+            "flag": True
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", self.url + "addtocart", headers=headers, data=payload)
+        return response.text
+    def viewAllTheProductsInTheCart(self,userToken):
+        payload = json.dumps({
+            "cookie": str(userToken),
             "flag": True
         })
         headers = {
